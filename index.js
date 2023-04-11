44010e94de98b0313a76b22aaa8b1a29832845bc
Mahek zehra <128639106+syedanaqvi72@users.noreply.github.com>
2023-04-11 04:14:37 +0000

    let , constant , var

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 console.log("hello world")
+let a = 67 //a contains 67
+console.log(a)
+a="mehak"
+console.log(a)
+const author= "mehak"
+let  d = null 
+
+let b = 54;
+let c = "java script";
+console.log(b)
+{
+  let b = " java script 23"
+  console.log(b)
+}
